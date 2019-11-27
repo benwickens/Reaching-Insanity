fde3b76480507c9c87942aab77fe906f7ca21de9
SimmyKR <57671614+SimmyKR@users.noreply.github.com>
2019-11-27 00:40:09 +0000

    Add files via upload
    syntax corrected for enum declarations

--- src/CellType.java
+++ src/CellType.java
@@ -1,9 +1,23 @@
 /**
- * Enum containing possible values of CellType
- * @author Yassine, Gursimran, Millie
+ * File name: CellType.java
+ *
+ * @version 1.1
+ * Creation Date: 18/11/2019
+ * Last Modification date: 23/11/2019
+ * @author Gursimran Randhawa, Yassine Abdalass, Millie Quinn
+ * <br>
+ * No copyright.
+ * <br>
+ * Purpose:
+ * List all of the cell types so that they can be used in other classes.
+ * <br>
+ * Version History
+ * 1.0 - list of all the cell types
+ * 1.1 - small change due to a syntax oversight.
+ *
  */
+
 public enum CellType {
-	
 	WALL,
 	EMPTY,
 	GOAL,
@@ -13,6 +27,5 @@ public enum CellType {
 	ICE,
 	RED_DOOR,
 	GREEN_DOOR,
-	BLUE_DOOR,
-
+	BLUE_DOOR
 }
--- src/Collectable.java
+++ src/Collectable.java
@@ -1,3 +1,20 @@
+/**
+ * File name: Collectable.java
+ *
+ * @version 1.1
+ * Creation Date: 18/11/2019
+ * Last Modification date: 23/11/2019
+ * @author Gursimran Randhawa, Yassine Abdalass, Millie Quinn
+ * <br>
+ * No copyright.
+ * <br>
+ * Purpose:
+ * List all of the collectables so that they can be used in other classes.
+ * <br>
+ * Version History
+ * 1.0 - list of all the collectables
+ * 1.1 - small change due to a syntax oversight.
+ */
 
 public enum Collectable {
 	TOKEN,
@@ -11,5 +28,4 @@ public enum Collectable {
 	SPEAR,
 	SHIELD,
 	GHOST_REPELLENT
-
 }
