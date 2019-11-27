5a07ae49acf7c908ec03dc6d6ccad27999a74843
SimmyKR <57671614+SimmyKR@users.noreply.github.com>
2019-11-27 00:42:09 +0000

    Add files via upload
    Missing constructor added - toString isn't implemented yet

--- src/Character.java
+++ src/Character.java
@@ -1,31 +1,54 @@
+/**
+ * File name: Character.java
+ *
+ * @version 1.1
+ * Creation Date: 18/11/2019
+ * Last Modification date: 26/11/2019
+ * @author Gursimran Randhawa, Yassine Abdalass, Millie Quinn
+ * <br>
+ * No copyright.
+ * <br>
+ * Purpose:
+ * Holds the behaviours and attributes that characters have.
+ * <br>
+ * Version History
+ * 1.0 - structure of the class made
+ * 1.1 - changed to implement more methods.
+ */
 
 public abstract class Character {
 	
-	protected int x;
-	protected int y;
-	
+	protected int xLocation;
+	protected int yLocation;
+
+	public Character(int xLocation, int yLocation){
+		this.xLocation = xLocation;
+		this.yLocation = yLocation;
+	}
+
+	public int getXLocation(){
+		 return xLocation;
+	}
+
+	public void setXLocation(int xLocation){
+		this.xLocation = xLocation;
+	}
+
+	public int getYLocation(){
+		return yLocation;
+	}
+
+	public void setYLocation(int yLocation){
+		this.yLocation = yLocation;
+	}
+
 	public void moveTo(int exactX, int exactY) {
-		x = exactX;
-		y = exactY;
+		xLocation = exactX;
+		yLocation = exactY;
 	}
 	
 	public String toString() {
+		//Requires implementation
 		return null;
-	}
-
-	public Integer getX() {
-		return x;
-	}
-
-	public void setX(Integer x) {
-		this.x = x;
-	}
-
-	public Integer getY() {
-		return y;
-	}
-
-	public void setY(Integer y) {
-		this.y = y;
 	}
 }
