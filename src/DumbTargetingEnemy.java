f542a24044f04357f0f2a84a10c91263081d5b05
millieq204 <57375328+millieq204@users.noreply.github.com>
2019-11-28 19:10:27 +0000

    start of DTE

--- src/DumbTargetingEnemy.java
+++ src/DumbTargetingEnemy.java
@@ -1,2 +1,37 @@
-public class DumbTargetingEnemy {
+/**
+ * File name: DumbTargetingEnemy.java
+ *
+ * @version 1.0
+ * Creation Date: 27/11/2019
+ * Last Modification date: 28/11/2019
+ * @author Millie Quinn
+ * <br>
+ * No copyright.
+ * <br>
+ * Purpose:
+ * Cover the implementation of the move method for all
+ * possible directions that a DumbTargetingEnemy can move in.
+ * <br>
+ * Version History
+ * 1.0 - basic structure and methods laid out
+ *
+ */
+public class DumbTargetingEnemy extends Character{
+	/*This enemy always moves directly towards the player in the
+	 * direction that wpuld make it get closer to the player
+	 *this is a very dumb enemy and easily gets stuck
+	 */
+	private Direction currentDirection;
+	
+	
+	public DumbTargetingEnemy(int x, int y, Direction currentDirection) {
+		super(x, y, "Enemy3.png");
+		this.currentDirection = currentDirection;
+	}
+	
+	//Find player
+	//calculate how far away player is
+	//move towards player
+	//cannot move if cell isn't empty
+	
 }
