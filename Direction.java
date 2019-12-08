aabf7e0da4daf2fb8b96aa5aef197760cf4b8d92
SimmyKR <57671614+SimmyKR@users.noreply.github.com>
2019-12-08 18:27:57 +0000

    JavaDoc

---
+++ Direction.java
@@ -0,0 +1,21 @@
+/**
+ * File name: Direction.java
+ * @version 1.0
+ * Creation Date: 26/11/19
+ * @author Alan Tollett
+ * <br>
+ * No copyright.
+ * <br>
+ * Purpose:
+ * Enum containing possible values of Direction (used by enemies and player movement methods)
+ * <br>
+ * Version History
+ * 1.0 - list of all the directions
+ */
+
+public enum Direction {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
